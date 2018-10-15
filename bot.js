87345fb9a6e981951a2c60c3bfeaed9d3a5b82c3
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-10-15 12:28:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,19 +28,6 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
 });
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '$'
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
 
 
 
