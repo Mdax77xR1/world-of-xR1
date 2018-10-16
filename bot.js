4510f00333bde0bf84eb4a1b5b48e0619c01472b
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-10-16 11:16:30 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -780,19 +780,6 @@ client.on("message", async msg => {
 });
 
 
-ar antispam = require("anti-spam");//npm i anti-spam
- 
-antispam(client, {
-  warnBuffer: 1, //الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على تحذير.
-  maxBuffer: 5, // الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على ميوت.
-  interval: 1000, // مقدار الوقت قبل حصول باند
-  warningMessage: "stop spamming.", // رسالة تحذير اذا سوا سبام!
-  roleMessage: "Muted!!", // الرسالة الي تجي اذا شخص اخذ ميوت
-  roleName: "Muted", // اسم رتبة الميوت
-  maxDuplicatesWarning: 3, // عدد الرسايل الي قبل التحذيرات
-  maxDuplicatesBan: 5, // عدد الرسايل الي يقدر المستخدم يرسلها قبل الميوت
-  time: 800, // عدد الوقت الي يجلس لين تسحب رتبة الميوت من الشخص الحسبة برمجية وليست كتابية 
-});
 
 
 
