9c2e08d490512b51ebd2ddce3ed8244a14e820e0
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-10-23 10:29:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -683,6 +683,27 @@ msg.channel.send({embed: embed})
 }
 });
 
+client.on('message', message => {
+                                if(!message.channel.guild) return;
+                        if (message.content.startsWith('ping')) {
+                            if(!message.channel.guild) return;
+                            var msg = `${Date.now() - message.createdTimestamp}`
+                            var api = `${Math.round(client.ping)}`
+                            if (message.author.bot) return;
+                        let embed = new Discord.RichEmbed()
+                        .setAuthor(message.author.username,message.author.avatarURL)
+                        .setColor('RANDOM')
+                        .addField('**Time Taken:**',msg + " ms 📶 ")
+                        .addField('**WebSocket:**',api + " ms 📶 ")
+         message.channel.send({embed:embed});
+                        }
+                    });
+
+
+
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);
