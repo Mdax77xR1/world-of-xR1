b0857d197072cfce8ff2a7aa506b0b70640697fd
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-10-23 11:08:47 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -775,7 +775,29 @@ client.on('message', msg => {
 });
 
 
+client.on('message', msg => {
+  if(msg.content === '$hide') {
+    msg.guild.channels.forEach(c => {
+      c.overwritePermissions(msg.guild.id, {
+        SEND_MESSAGES: false,
+        READ_MESSAGES: false
+      })
+    })
+    msg.channel.send('.')
+  }
+})
 
+client.on('message', msg => {
+  if(msg.content === '$unhide') {
+    msg.guild.channels.forEach(c => {
+      c.overwritePermissions(msg.guild.id, {
+        SEND_MESSAGES: true,
+        READ_MESSAGES: true
+      })
+    })
+    msg.channel.send('.')
+  }
+})
 
 
 
