ebbfd7b6d86ce97d4c604a8252e5abbf9fd4fe99
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-10-23 13:08:01 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -802,7 +802,7 @@ hours = 12;
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "Alpha Codes";
+    let copy = "xR1 Server";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
