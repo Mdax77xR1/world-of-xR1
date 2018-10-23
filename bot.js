956e74a77f605c34f72f0066d4f68d08afac2aae
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-10-23 10:59:19 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -758,7 +758,22 @@ client.on('message',message =>{
 
 
 
+const perfix = '$';
+client.on('message', msg => {
+ if (msg.content.startsWith(prefix + 'send')) {
+      let args = msg.content.split(' ').slice(1)
+      if (!args[0]) return msg.reply(`**منشن الشخص اولا**`)
+      if (!args[1]) return msg.reply(`**ما هي الرساله المطلوب ارسالها**`)
+      let alpha = msg.mentions.members.first()
+      if (!alpha) return msg.reply(`**يجب تحديد الشخص**`)
+      let alphaEmbed = new Discord.RichEmbed()
+      .setTitle(`**رسالة جديده لك من شخص ما**`)
+      .setDescription(args.join(" "))
 
+      client.users.get(`${alpha.id}`).send(alphaEmbed)
+      msg.reply(`**تم ارسال الرساله**`)
+    }
+});
 
 
 
