4d0d59fd2259bce8bf9e0ed9f00746589c06b350
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-02 11:33:57 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+const prefix = '$'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
