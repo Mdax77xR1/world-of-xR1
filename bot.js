d1af581d99957f6f76bdea7a90331ca1dc7df90e
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-02 11:21:21 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -288,7 +288,7 @@ client.on('message',  message => {
 
 client.on('message', message => {
  
-    if(message.content === "$mutechannel") {
+ if (message.content.startsWith('$mutechannel')) {
                         if(!message.channel.guild) return message.reply('** This command only for servers**');
  
 if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
@@ -299,8 +299,8 @@ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__
                message.reply("**__تم تقفيل الشات__ :white_check_mark: **")
            });
              }
-//FIRE BOT
- if(message.content === "$unmutechannel") {
+//xR1 Bot
+if (message.content.startsWith('$unmuetchannel')) {
                      if(!message.channel.guild) return message.reply('** This command only for servers**');
  
 if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
