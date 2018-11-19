a40162ef0b8e9577693ff609a5edaff8c10b10ec
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-19 08:48:35 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -919,18 +919,26 @@ client.on('guildMemberAdd', member => {
 
 
 
-client.on('guildMemberAdd', member => {
-    const botCount = member.guild.members.filter(m=>m.user.bot).size
-    const memberCount = [member.guild.memberCount] - [botCount]
-    client.channels.get('514117001418113054').setName(`⟫『 ${memberCount} عدد الاعضاء 』⟪`);
-    client.channels.get('514117081067814922').setName(`⟫『 ${botCount} عدد البوتات 』⟪`);
-});
-
-client.on('guildMemberRemove', member => {
-    const botCount = member.guild.members.filter(m=>m.user.bot).size
-    const memberCount = [member.guild.memberCount] - [botCount]
-    client.channels.get('514117001418113054').setName(`⟫『 ${memberCount} عدد الاعضاء 』⟪`);
-    client.channels.get('514117081067814922').setName(`⟫『 ${botCount} عدد البوتات 』⟪`);
+client.on('message',async msg => {
+  var p = "$";
+  if(msg.content.startsWith("$user")) {
+  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
+  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
+  msg.guild.createChannel(`يتم تحضير الروم :[]` , 'voice').then(time => {
+    time.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+  setInterval(() => {
+      var currentTime = new Date(),
+Year = currentTime.getFullYear(),
+Month = currentTime.getMonth() + 1,
+Dat = currentTime.getDate()
+      time.setName(`Members : ◤ → ${client.users.size} ← ◢`);
+ },1000);
+  });
+  }
+ 
 });
 
 
