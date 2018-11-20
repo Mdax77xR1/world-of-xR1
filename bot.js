8b20cefe3ae4aa94dab0c92bc052f44db06ec57e
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-20 11:25:57 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1009,7 +1009,6 @@ if (message.content.startsWith(adminprefix + 'sett')) {
 
 
 
-ar fs = require("fs");
 let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
 client.on("message", message => {
   if (message.author.bot) return;
