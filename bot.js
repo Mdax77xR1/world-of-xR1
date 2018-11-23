791349fd8c6c3abeb2724f94887f91a4666f6e8e
Mdax77xR1 <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-23 13:24:35 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1116,6 +1116,68 @@ client.on ("guildMemberRemove", member => {
 
 
 
+let points = JSON.parse(fs.readFileSync('./fkkPTS.json', 'utf8'));
+const prefix = "$";
+ 
+client.on('message', message => {
+if (!points[message.author.id]) points[message.author.id] = {
+    points: 0,
+  };
+if (message.content.startsWith(prefix + 'فكك')) {
+    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+ 
+const type = require('./fkk.json');
+const item = type[Math.floor(Math.random() * type.length)];
+const filter = response => {
+    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+};
+message.channel.send('**لديك 15 ثانيه لتفكيك الكلمه**').then(msg => {
+ 
+           
+msg.channel.send(`${item.type}`).then(() => {
+        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
+        .then((collected) => {
+        message.channel.send(`${collected.first().author} ✅ **احسنت لقد تمكنت من تفكيك الكلمه بسرعه**`);
+        console.log(`[Typing] ${collected.first().author} typed the word.`);
+            let won = collected.first().author;
+            points[won.id].points++;
+          })
+          .catch(collected => {
+            message.channel.send(`:x: **لم يتمكن احد من تفكيك الكلمه في الوقت المناسب**`);
+            console.log('[Typing] Error: No one type the word.');
+          })
+        })
+    })
+}
+});
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'نقاطي')) {
+    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
+    let userData = points[message.author.id];
+    let embed = new Discord.RichEmbed()
+    .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+    .setColor('#000000')
+    .setDescription(`نقاطك: \`${userData.points}\``)
+    message.channel.sendEmbed(embed)
+  }
+  fs.writeFile("./fkkPTS.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
