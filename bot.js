9ac5b5b160b695711918d7ae3aa9222f1e1fad09
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2018-11-30 05:05:22 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 
 const Discord = require('discord.js');
 const fs = require("fs");
-const vpoints = require("vpoints");
 const jimp = require("jimp");
 const client = new Discord.Client();
 client.on('ready', () => {
