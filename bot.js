c4c63aa5a9e6775d6c24ffea8ddc882ceae1a292
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2018-12-04 10:21:53 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -413,7 +413,6 @@ if (message.content === '$help') {
       .addField("**❖ $sara7a**","**لعبة صراحه**")
       .addField("**❖ $rps**","**لعبة حجرة ورقة مقص**")
       .addField("**❖ $cuttwet**","**لعبة كت  تويت**")
-      .addField("**❖  $marry**","** لعبة الزواج  **")
       .addField("**❖  $فكك**","**لعبة فكك **")
       .addField("**۩ஜ▬▬▬▬▬▬✦Info✦▬▬▬▬▬▬ஜ۩**","** **")
       .addField("**❖ --->**","**Made By : ! - Mdax .#0518**")
@@ -1215,47 +1214,6 @@ client.on("message", message => {
      });
     }
 });
-
-
-
-
-
-
-
-
-
-client.on('message', message => {
-      if(message.content.startsWith ("$marry")) {
-      if(!message.channel.guild) return message.reply('** This command only for servers **')
-      var proposed = message.mentions.members.first()
-     
-      if(!message.mentions.members.first()) return message.reply(' 😏 **لازم تطلب ايد وحدة**').catch(console.error);
-      if(message.mentions.users.size > 1) return message.reply(' 😳 **ولد ما يصحلك الا حرمة وحدة كل مرة**').catch(console.error);
-       if(proposed === message.author) return message.reply(`**خنثى ؟ **`);
-        if(proposed === client.user) return message.reply(`** تبي تتزوجني؟ **`);
-              message.channel.send(`**${proposed} 
- بدك تقبلي عرض الزواج من ${message.author} 
- العرض لمدة 15 ثانية  
- اكتبي موافقة او لا**`)
-
-const filter = m => m.content.startsWith("موافقة");
-message.channel.awaitMessages(filter, { max: 1, time: 15000, errors: ['time'] })
-.then(collected =>{ 
-    message.channel.send(` **${message.author} و ${proposed} الف الف مبروك الله , يرزقكم الذرية الصالحة** `);
-})
-
-   const filte = m => m.content.startsWith("لا");
-message.channel.awaitMessages(filte, { max: 1, time: 15000, errors: ['time'] })
-.then(collected =>{ 
-   message.channel.send(`  **${message.author} تم رفض عرضك** `);
-})
-        
-  }
-});
-
-
-
-
 
 
 
