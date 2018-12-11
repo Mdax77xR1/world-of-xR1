10be8e5121cfac6e32422b94e24cd29d121df0fc
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2018-12-11 03:29:25 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     xR1Server' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$help $inv`,"https://www.twitch.tv/Mdax77xR1")
+client.user.setGame(`xR1 Server`,"https://www.twitch.tv/Mdax77xR1")
 client.user.setStatus("online")
  
 });
