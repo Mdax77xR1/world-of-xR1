03ed0d569f8280a6cb60be3dcbd385ca8abb280f
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2019-01-01 14:19:55 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1182,7 +1182,28 @@ client.on("guildMemberAdd", async member => {
   });
 
 
+client.on('message',async message => {
+  if(message.content === '$banall') {
+    var user = message.mentions.users.first();
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('❌|**\`ADMINISTRATOR\`لا توجد لديك صلاحية `**');
+    if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+    const guild = message.guild;
 
+  message.guild.fetchBans().then(ba => {
+  ba.forEach(ns => {
+message.guild.unban(ns);
+  const embed= new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setAuthor("Succes!", "https://images-ext-1.discordapp.net/external/vp2vj9m0ieU5J6SHg6ObIsGpTJyoZnGAebrd0_vi848/https/i.imgur.com/GnR2unD.png?width=455&height=455")
+        .setDescription(`**✅ Has Been Unban For All**`)
+    .setFooter('Requested by '+message.author.username, message.author.avatarURL)
+  message.channel.sendEmbed(embed);
+  guild.owner.send(`سيرفر : ${guild.name}
+  **تم تبنيد  الجميع بواسطة** : <@${message.author.id}>`) 
+  });
+  });
+  }
+  });
 
 
 
